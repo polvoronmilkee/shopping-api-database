8b8fdbc8834ee3d15f5c8710a6bd0c792db999c5
Sophia Marielle C. Mendoza <sophiamarielle.mendoza-24@cpu.edu.ph>
2026-02-10 16:35:23 +0800

    env

---
+++ __tests__/utils/db.ts
@@ -0,0 +1,21 @@
+import { createClient } from "@supabase/supabase-js";
+import dotenv from "dotenv";
+
+dotenv.config({ path: ".env.test" });
+
+const supabaseUrl = process.env.SUPABASE_URL!;
+const supabaseKey = process.env.SUPABASE_KEY!
+
+export const testClient = createClient(supabaseUrl, supabaseKey);
+
+const clearSupabase = async () => {
+    const { error } = await testClient
+        .from('cart_items')
+        .delete()
+        .neq('id', 0); // Delete all rows
+
+
+    if (error) {
+        throw new Error(`Failed to clear cart_items table: ${error.message}`);
+    }
+}
